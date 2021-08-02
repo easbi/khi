4679fc8651fe341b6af682acac33fcf7a142341c
easterikhxen <easterikhxen@gmail.com>
2021-08-02 12:42:33 +0700

    perbaikan formasi datatable

--- resources/views/dailyactivity/index.blade.php
+++ resources/views/dailyactivity/index.blade.php
@@ -25,45 +25,45 @@
 				<p>{{ $message }}</p>
             </div>
             @endif
-			<div class="card-body p-0 pb-3 text-center">
-				<table id="example"  class="display responsive nowrap" width="100%">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Pegawai</th>
-                                    <th>Tanggal</th>
-                                    <th>Status</th>
-                                    <th>Kegiatan</th>
-                                    <th>Pemakaian Internet</th>
-                                    <th>Progress Kegiatan</th>
-                                    <th>Aksi</th>
-                            	</tr>    
-                            </thead>
-                            <tbody>
-                                @foreach ($activities as $act)
-                                <tr>
-                                    <td>{{ ++$i }}</td>
-                                    <td>{{ $act->nip }}</td>
-                                    <td>{{ $act->created_at }}</td>
-                                    <td>{{ $act->wfo_wfh }}</td>
-                                    <td>{{ $act->kegiatan }}</td>
-                                    <td>{{ $act->is_internet }}</td>
-                                    <td>{{ $act->kualitas }}</td>
-                                    <td>
-                                        <form action="{{ route('act.destroy',$act->id) }}" method="POST">
-                         
-                                            <a class="btn btn-info btn-sm" href="{{ route('act.show',$act->id) }}">Show</a>
-                                            <a class="btn btn-primary btn-sm" href="{{ route('act.edit',$act->id) }}">Edit</a>                         
-                                            @csrf
-                                            @method('DELETE')                         
-                                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-			</div>
+            <div class="card-body d-flex flex-column">
+                <table id="example"  class="display responsive nowrap" width="100%">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Pegawai</th>
+                            <th>Tanggal</th>
+                            <th>Status</th>
+                            <th>Kegiatan</th>
+                            <th>Pemakaian Internet</th>
+                            <th>Progress Kegiatan</th>
+                            <th>Aksi</th>
+                        </tr>    
+                    </thead>
+                    <tbody>
+                        @foreach ($activities as $act)
+                        <tr>
+                            <td>{{ ++$i }}</td>
+                            <td>{{ $act->nip }}</td>
+                            <td>{{ $act->created_at }}</td>
+                            <td>{{ $act->wfo_wfh }}</td>
+                            <td>{{ $act->kegiatan }}</td>
+                            <td>{{ $act->is_internet }}</td>
+                            <td>{{ $act->kualitas }}</td>
+                            <td>
+                                <form action="{{ route('act.destroy',$act->id) }}" method="POST">
+
+                                    <a class="btn btn-info btn-sm" href="{{ route('act.show',$act->id) }}">Show</a>
+                                    <a class="btn btn-primary btn-sm" href="{{ route('act.edit',$act->id) }}">Edit</a>                         
+                                    @csrf
+                                    @method('DELETE')                         
+                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>                        
+            </div>
 		</div>
 	</div>
 </div>
